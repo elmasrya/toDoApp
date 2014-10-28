f1467a480a27fcaa51951a47fb8ec2565695f5a7
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-28 19:33:18 -0400

    append working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,30 @@
 var toDo = function(options){
   var options=options || {};
+  this.listItem= options.listItem;
   this.name=options.name;
   this.status=options.status || 'incomplete';
 }
 
-var item = new toDo({
-  name: "item1", 
+var item;
+var itemArray=[];
+
+
+var itemTemplate=$('#template').html();
+var render = _.template(itemTemplate);
+
+ $('#add').on('submit', function(event){
+   event.preventDefault();
+
+   var inputVal=$('#text').val();
+
+
+  item = new toDo({
+     name: inputVal,
+     listItem: $(render({name: inputVal}))[0]
+
+   });
+
+  itemArray.push(item);
+
+  $('.list').append(item.listItem);
 });
