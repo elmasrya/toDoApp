406439fa4caa1bf4465c56ede8339b4d57f30c9d
rdanieldesign <rdanieldesign@gmail.com>
2014-10-28 16:35:37 -0400

    List layout started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,5 +5,23 @@ var toDo = function(options){
 }
 
 var item = new toDo({
-  name: "item1", 
+  name: "item1",
 });
+
+var contWidth = ($('.container').width());
+var contHeight = ($('.container').height()) - $('.controls').height();
+var item = $('.list li');
+var columns = 7;
+var rows = 2;
+var itemWidth;
+var itemHeight;
+
+// Set Interval to adjust item size based on Container size
+setInterval(function(){
+  contWidth = ($('.container').width());
+  contHeight = ($('.container').height()) - $('.controls').height();
+  itemWidth = contWidth / columns;
+  itemHeight = contHeight / rows;
+  $(item).css({'height' : itemHeight, 'width': itemHeight});
+  console.log()
+}, 100)
