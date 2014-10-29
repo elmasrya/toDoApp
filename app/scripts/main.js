a59ff3f1c03e94e8ae29f20ce0791f87af056c41
rdanieldesign <rdanieldesign@gmail.com>
2014-10-29 18:17:54 -0400

    Filter function done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,6 +53,7 @@ $('#add').on('submit', function(event){
 
      // Store newly created item instance in an array, 'itemArray'
     itemArray.push(data);
+    $('.totalCount').html(itemArray.length);
 
     $('.list').append(render(data));
 
@@ -72,8 +73,7 @@ $('.list').on('click', 'li', function(event){
   event.preventDefault();
 
   var itemID = $(this).attr('id');
-
-  $(this).toggleClass('complete');
+  var itemClicked = this;
 
   // Find the object in the itemArray array that has matching values to the one clicked on.
   markedItem = _.findWhere(itemArray, { _id: itemID });
@@ -84,15 +84,14 @@ $('.list').on('click', 'li', function(event){
     compCount++;
     $('.compCount').html(compCount);
     $('.totalCount').html(itemArray.length);
-
-
+    $(itemClicked).addClass('complete').removeClass('incomplete');
   }
   else {
     markedItem.status = 'incomplete';
     compCount--;
     $('.compCount').html(compCount);
     $('.totalCount').html(itemArray.length);
-
+    $(itemClicked).addClass('incomplete').removeClass('complete');
   }
 
   $.ajax({
@@ -105,26 +104,34 @@ $('.list').on('click', 'li', function(event){
 
 $('#removeComp').on('click', function() {
 
-  // completedItems = _.findWhere(itemArray, { status:'complete' });
-
-
-  // console.log(completedItems);
-
   _.each(itemArray, function(item){
 
-  var compId = item._id;
+    var compId = item._id;
 
-  if (item.status==='complete'){
-  $.ajax({
-    type: 'DELETE',
-    url: toDoServer + '/' + compId,
-    data: item
+    if (item.status==='complete'){
+      $.ajax({
+        type: 'DELETE',
+        url: toDoServer + '/' + compId,
+        data: item
+      });
+    };
+
   });
-};
-
-
-});
 
   $('.complete').remove();/*This will remove all the list items with the class of complete*/
 
+});
+
+// Filtering
+$('#completeF').on('click', function(){
+  $('.incomplete').css('display', 'none');
+  $('.complete').css('display', 'inline-block');
+});
+$('#activeF').on('click', function(){
+  $('.complete').css('display', 'none');
+  $('.incomplete').css('display', 'inline-block');
+});
+$('#allF').on('click', function(){
+  $('.incomplete').css('display', 'inline-block');
+  $('.complete').css('display', 'inline-block');
 });
