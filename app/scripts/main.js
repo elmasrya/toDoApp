6ee75a9cc336c14cde03793232e72bedf29cc2f6
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-28 20:09:01 -0400

    alert finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,6 +17,12 @@ var render = _.template(itemTemplate);
    event.preventDefault();
 
    var inputVal=$('#text').val();
+   if(inputVal==0) {
+     alert("You must enter an input")
+   } else{
+
+
+
 
 
   item = new toDo({
@@ -28,8 +34,7 @@ var render = _.template(itemTemplate);
   itemArray.push(item);
 
   $('.list').append(item.listItem);
-
-
+}
 });
 
 var contWidth = ($('.container').width());
