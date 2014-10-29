b33886188a199c01c0ce5b5a7ca2392c645cff0b
rdanieldesign <rdanieldesign@gmail.com>
2014-10-28 20:50:57 -0400

    Finish status change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,11 @@
+// Styling new list items
 var contWidth = ($('.container').width());
 var contHeight = ($('.container').height()) - $('.controls').height();
-var item = $('.list li');
+var newItem = $('.list li');
 var columns = 7;
 var rows = 2;
 var itemWidth;
 var itemHeight;
-
-// Set Interval to adjust item size based on Container size
-setInterval(function(){
-  contWidth = ($('.container').width());
-  contHeight = ($('.container').height()) - $('.controls').height();
-  itemWidth = contWidth / columns;
-  itemHeight = contHeight / rows;
-  $(item).css({'height' : itemHeight, 'width': itemHeight});
-  console.log()
-}, 100)
 
 var toDo = function(options){
   var options=options || {};
@@ -31,17 +22,10 @@ var itemArray=[];
 var itemTemplate=$('#template').html();
 var render = _.template(itemTemplate);
 
- $('#add').on('submit', function(event){
+$('#add').on('submit', function(event){
    event.preventDefault();
 
    var inputVal=$('#text').val();
-   if(inputVal==0) {
-     alert("You must enter an input")
-   } else{
-
-
-
-
 
   item = new toDo({
      name: inputVal,
@@ -52,5 +36,31 @@ var render = _.template(itemTemplate);
   itemArray.push(item);
 
   $('.list').append(item.listItem);
-}
+
+  // Reset my form
+  $(this)[0].reset();
+
+  // Set Interval to adjust item size based on Container size
+  setInterval(function(){
+    contWidth = ($('.container').width());
+    contHeight = ($('.container').height()) - $('.controls').height();
+    itemWidth = contWidth / columns;
+    itemHeight = contHeight / rows;
+    $(newItem).css({'height' : itemHeight + 'px', 'width': itemHeight + 'px'});
+  }, 100);
+
+  // Mark as completed
+  $('.list').on('click', 'li', function(){
+
+    markedItem = _.findWhere(itemArray, { listItem: $(this)[0] });
+
+    if(markedItem.status === 'incomplete'){
+      $(this).css('background-color', 'red');
+      markedItem.status = 'complete';
+    }
+    else {
+      $(this).css('background-color', 'green');
+      markedItem.status = 'incomplete';
+    };
+  });
 });
