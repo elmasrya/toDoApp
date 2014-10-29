1c947011e691d92268669c64050414453dcc66e3
rdanieldesign <rdanieldesign@gmail.com>
2014-10-29 13:41:33 -0400

    Connect to server, put items

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var toDoServer = 'http://tiy-atl-fe-server.herokuapp.com/collections/ARtoDoApp';
+
 // Styling new list items
 var contWidth = ($('.container').width());
 var contHeight = ($('.container').height()) - $('.controls').height();
@@ -9,39 +11,54 @@ var itemHeight;
 
 var toDo = function(options){
   var options=options || {};
-  this.listItem= options.listItem;
   this.name=options.name;
-  this.status=options.status;
-
+  this.status=options.status || 'incomplete';
 };
 
 
-
 var item;
-var itemArray=[];
+var itemArray;
 
 
 var itemTemplate=$('#template').html();
 var render = _.template(itemTemplate);
 
+// Put all exisiting toDo items on page
+$.getJSON(toDoServer).done( function(data){
+  itemArray = data;
+  _.each(itemArray, function(item){
+    $('.list').append(render(item));
+  });
+});
+
 $('#add').on('submit', function(event){
    event.preventDefault();
 
+   var inputField = this;
+
    var inputVal=$('#text').val();
 
-  item = new toDo({
-     name: inputVal,
-     listItem: $(render({name: inputVal}))[0]
-
+   // Create a new instance
+   item = new toDo({
+     name: inputVal
    });
 
-   // Store newly created item instance in an array, 'itemArray'
-  itemArray.push(item);
+   // Send item to server
+   $.ajax({
+     type: 'POST',
+     url: toDoServer,
+     data: item
+   }).done( function(data){
 
-  $('.list').append(item.listItem);
+     // Store newly created item instance in an array, 'itemArray'
+    itemArray.push(data);
 
-  // Reset my form
-  $(this)[0].reset();
+    $('.list').append(render(data));
+
+    // Reset my form
+    $(inputField)[0].reset();
+
+   });
 
   // Set Interval to adjust item size based on Container size
   setInterval(function(){
@@ -54,29 +71,40 @@ $('#add').on('submit', function(event){
 
 });
 
+var markedItem;
 
 // Mark as completed
-$('.list').on('click', 'li', function(){
-  // event.preventDefault();
+$('.list').on('click', 'li', function(event){
+
+  event.preventDefault();
+
+  var itemID = $(this).attr('id');
 
   $(this).toggleClass('complete');
 
   // Find the object in the itemArray array that has matching values to the one clicked on.
-  markedItem = _.findWhere(itemArray, { listItem: $(this)[0] });
+  markedItem = _.findWhere(itemArray, { _id: itemID });
 
   // if incomplete, mark as complete. Otherwise, mark as incomplete.
   if(markedItem.status === 'incomplete'){
-    this.status = 'complete';
-    console.log(this);
+    markedItem.status = 'complete';
+    console.log(markedItem);
   }
   else {
-    this.status = 'incomplete';
-    console.log(this);
+    markedItem.status = 'incomplete';
+    console.log(markedItem);
   }
+
+  $.ajax({
+    type: 'PUT',
+    url: toDoServer + '/' + markedItem._id,
+    data: markedItem
+  });
+
 });
 
-  $('#removeComp').on('click', function() {
+$('#removeComp').on('click', function() {
 
-    $('.complete').remove();/*This will remove all the list items with the class of complete*/
+  $('.complete').remove();/*This will remove all the list items with the class of complete*/
 
-  });
+});
