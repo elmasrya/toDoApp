38a19111bba77dd3c40821e9b261e9a1c0fbbe1e
rdanieldesign <rdanieldesign@gmail.com>
2014-10-28 20:10:56 -0400

    Pull

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,21 @@
+var contWidth = ($('.container').width());
+var contHeight = ($('.container').height()) - $('.controls').height();
+var item = $('.list li');
+var columns = 7;
+var rows = 2;
+var itemWidth;
+var itemHeight;
+
+// Set Interval to adjust item size based on Container size
+setInterval(function(){
+  contWidth = ($('.container').width());
+  contHeight = ($('.container').height()) - $('.controls').height();
+  itemWidth = contWidth / columns;
+  itemHeight = contHeight / rows;
+  $(item).css({'height' : itemHeight, 'width': itemHeight});
+  console.log()
+}, 100)
+
 var toDo = function(options){
   var options=options || {};
   this.listItem= options.listItem;
@@ -31,21 +49,3 @@ var render = _.template(itemTemplate);
 
 
 });
-
-var contWidth = ($('.container').width());
-var contHeight = ($('.container').height()) - $('.controls').height();
-var item = $('.list li');
-var columns = 7;
-var rows = 2;
-var itemWidth;
-var itemHeight;
-
-// Set Interval to adjust item size based on Container size
-setInterval(function(){
-  contWidth = ($('.container').width());
-  contHeight = ($('.container').height()) - $('.controls').height();
-  itemWidth = contWidth / columns;
-  itemHeight = contHeight / rows;
-  $(item).css({'height' : itemHeight, 'width': itemHeight});
-  console.log()
-}, 100)
